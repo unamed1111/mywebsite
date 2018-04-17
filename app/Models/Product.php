d6c08e90a5083c9f0344da58afe0e87e7c40d41d
Tudm <unamed11111@gmail.com>
2018-04-17 11:03:48 +0700

    add model product + detail

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['product_name','price','trade_mark_id','categories_id','description'];
+
+    public function trademark()
+    {
+    	return $this->belongsTo(TradeMark::class,'trade_mark_id','id');
+    }
+
+    public function category()
+    {
+    	return $this->belongsTo(Category::class,'categories_id','id');
+    }
+
+    public function detail()
+    {
+    	return $this->hasOne(ProductDetail::class);
+    }
 }
